a5e49e206f27713b814eb7268e901658bd8d729e
Farizzz <113644816+FarizzzAPK@users.noreply.github.com>
2026-01-19 20:21:37 +0200

    product deatils UI  almost finished

--- lib/features/product/views/product_details_view.dart
+++ lib/features/product/views/product_details_view.dart
@@ -1,14 +1,99 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hungry/core/constants/app_constants.dart';
+import 'package:hungry/features/product/widgets/Custom_Topping.dart';
+import 'package:hungry/features/product/widgets/Custom_option.dart';
+import 'package:hungry/features/product/widgets/spicy_slider.dart';
+import 'package:hungry/shared/custom_text.dart';
 
-class ProductDetailsView extends StatelessWidget {
-  const ProductDetailsView ({super.key});
+
+class ProductDetailsView extends StatefulWidget {
+  const ProductDetailsView({super.key});
+
+  @override
+  State<ProductDetailsView> createState() => _ProductDetailsViewState();
+}
+
+class _ProductDetailsViewState extends State<ProductDetailsView> {
+  double value = 0.7;
+  List<String> toppings = [
+    "Tomato",
+    "Onions",
+    "Pickles",
+    "Bacons",
+  ];
+
+  List<String> options = [
+    "Fries",
+    "Coleslaw",
+    "Salad",
+    "Onion",
+  ];
+
+  List<String> topping_image = [
+    "assets/images/pngwing15.png",
+    "assets/images/pngwing17.png",
+    "assets/images/pngwing18.png",
+    "assets/images/pngwing19.png",
+  ];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_rounded, weight: 700, size: 25),
+          onPressed: () => Navigator.pop(context),
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor:AppConstants().PrimaryColor, 
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SpicySlider(
+              value: value,
+              onChanged: (v) {
+              setState(() {
+                value = v;},
+                );
+            },),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: CustomText(text: "Toppings",weight: FontWeight.bold,),
+            ),
+ SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(toppings.length, (index) {
+
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CustomTopping(image: topping_image[index], topping_name: toppings[index]),
+                  );}),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: CustomText(text: "Side options",weight: FontWeight.bold,),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(options.length, (index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CustomOption(image: topping_image[index], option_name: options[index]),
+                  );}),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/features/product/widgets/spicy_slider.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:hungry/core/constants/app_constants.dart';
+import 'package:hungry/shared/custom_text.dart';
+
+class SpicySlider extends StatefulWidget {
+   SpicySlider({super.key, required this.value, required this.onChanged});
+  final double value;
+  final ValueChanged<double> onChanged;
+
+  @override
+  State<SpicySlider> createState() => _SpicySliderState();
+}
+
+class _SpicySliderState extends State<SpicySlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/pngwing12.png", 
+                  height: 250,
+                  errorBuilder: (context, error, stackTrace) => const Icon(Icons.fastfood, size: 100),
+                ),
+                
+                const SizedBox(width: 16),
+
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Customize Your Burger\nto Your Tastes. Ultimate Experience",
+                        style: TextStyle(
+                          color: Color(0xff3C2F2F),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      CustomText(text: "Spicy", weight: FontWeight.bold,),
+                      SliderTheme(
+                        data: SliderTheme.of(context).copyWith(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          trackHeight: 5,
+
+                        ),
+                        child: Slider(
+                          
+                          max: 1,
+                          min: 0,
+                          value:widget.value,
+                          onChanged: widget.onChanged,
+                          activeColor:AppConstants().PrimaryColor,
+                        ),
+                      ),
+
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("🥶", style: TextStyle(fontSize: 16)),
+                            Text("🌶", style: TextStyle(fontSize: 16)),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            );
+  }
+}
